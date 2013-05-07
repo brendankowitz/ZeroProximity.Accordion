6313f642476de3247bd5b220f0f71a49e663e2ee
Brendan Kowitz <brendan.kowitz@gmail.com>
2013-05-07 23:59:41 +1000

    Initial Commit

---
+++ ZeroProximity.Accordion.WinStore/ExpandDirection.cs
@@ -0,0 +1,34 @@
+﻿namespace ZeroProximity.Controls
+{
+    /// <summary>
+    /// Specifies the direction in which an
+    /// <see cref="T:System.Windows.Controls.Expander" /> control opens.
+    /// </summary>
+    /// <QualityBand>Stable</QualityBand>
+    public enum ExpandDirection
+    {
+        /// <summary>
+        /// Specifies that the <see cref="T:System.Windows.Controls.Expander" />
+        /// control opens in the down direction.
+        /// </summary>
+        Down = 0,
+
+        /// <summary>
+        /// Specifies that the <see cref="T:System.Windows.Controls.Expander" />
+        /// control opens in the up direction.
+        /// </summary>
+        Up = 1,
+
+        /// <summary>
+        /// Specifies that the <see cref="T:System.Windows.Controls.Expander" />
+        /// control opens in the left direction.
+        /// </summary>
+        Left = 2,
+
+        /// <summary>
+        /// Specifies that the <see cref="T:System.Windows.Controls.Expander" />
+        /// control opens in the right direction.
+        /// </summary>
+        Right = 3,
+    }
+}
