19d9255d4813753a83f028039e63557e37845c78
Brendan Kowitz <brendan.kowitz@gmail.com>
2013-05-17 08:50:43 +1000

    large list problem

--- Source/Solution Items/GlobalAssemblyInfo.cs
+++ Source/Solution Items/GlobalAssemblyInfo.cs
@@ -12,5 +12,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("1.0.0.4")]
-[assembly: AssemblyFileVersion("1.0.0.4")]
+[assembly: AssemblyVersion("1.0.0.5")]
+[assembly: AssemblyFileVersion("1.0.0.5")]
